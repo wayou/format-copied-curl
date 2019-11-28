f67e35f6e3bf8611fe8baf19a5e9e2f59d9e59da
wayou <liuwayong@gmail.com>
2019-11-29 00:14:29 +0800

    add gh-pages and setup deploy

--- src/common/format.ts
+++ src/common/format.ts
@@ -3,5 +3,8 @@
  * @param input original curl content
  */
 export const format = (input: string) => {
-    return input.replace(/(\s-[A-Z]|--\b\w*\b)(\s\'(.*?)\')?/gm,(match)=>match+" \\\n");
+  return input.replace(
+    /(\s-[A-Z]|--\b\w*\b)(\s\'(.*?)\')?/gm,
+    match => match + " \\\n"
+  );
 };
