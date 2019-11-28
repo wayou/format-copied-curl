290fb4aaba7fb28305826605aa4a285cec137739
wayou <liuwayong@gmail.com>
2019-11-28 23:49:39 +0800

    add build

---
+++ build/precache-manifest.709bb518917f5d716ba8617ca8569be6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4b50bd1f07f338a44fa12ec69e86b84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fd8e0ff2d80a63ae6b6",
+    "url": "/static/css/main.45e8e369.chunk.css"
+  },
+  {
+    "revision": "a71e11e1b290d430ff7e",
+    "url": "/static/js/2.dd435ffb.chunk.js"
+  },
+  {
+    "revision": "1fd8e0ff2d80a63ae6b6",
+    "url": "/static/js/main.664d1a06.chunk.js"
+  },
+  {
+    "revision": "b609e7d5e7de79e3c8c5",
+    "url": "/static/js/runtime-main.d207578b.js"
+  }
+]);
